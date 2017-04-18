85d9ac6adeb63fa9f1055fe205c2501bcaf87738
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-04-18 09:52:06 +0200

    features extraction : extraction by windows (samples creation)

--- Scripts/features_extraction.m
+++ Scripts/features_extraction.m
@@ -1,11 +1,12 @@
-function features = features_extraction(easy,hard,hard_assistance,header,window_size,step_size)
+function features_list = features_extraction(easy,hard,hard_assistance,header,window_size,step_size)
     
     splitted_easy = split(easy,window_size,step_size);
     splitted_hard = split(hard,window_size,step_size);
     splitted_hard_assist = split(hard_assistance,window_size,step_size);
     
     FeatEasy = extract_feature_of_matrix(splitted_easy,windows_size,0);
-    FeatHard = extract_feature_of_matrix(splitted_easy,windows_size,1);
-    FeatHardAssist = extract_feature_of_matrix(splitted_easy,windows_size,2);
+    FeatHard = extract_feature_of_matrix(splitted_hard,windows_size,1);
+    FeatHardAssist = extract_feature_of_matrix(splitted_hard_assist,windows_size,2);
     
+    features_list = vertcat(FeatEasy,FeatHard,FeatHardAssist);
     
