58cccd75f7f393f77fe11e872480ffa80adcf437
chobaz <loic.chobaz@bluewin.ch>
2017-05-04 11:13:34 +0200

    correction

--- Scripts/electrodes_selection.m
+++ Scripts/electrodes_selection.m
@@ -1,7 +1,10 @@
-function data=electrodes_selection(signal,header,label)
+function [data, new_header]=electrodes_selection(signal,header,label)
+%select electrodes of interest
+%INPUT : signal, header, label
+%OUTPUT : data is the signal for each electrodes selected
 
 idx=index_of_electrodes(label,header);
 
 data=signal(idx,:);
-
+new_header=header(idx,:);
 end
