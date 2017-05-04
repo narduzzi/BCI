36cc73232fc730ddad0e58539ef4d3db10424056
chobaz <loic.chobaz@bluewin.ch>
2017-05-04 10:48:40 +0200

    electrodes selection

---
+++ Scripts/electrodes25.m
@@ -0,0 +1,8 @@
+clear all
+clc
+close all
+
+label={'F3','F1','Fz','F2','F4','FC3','FC1','FCz','FC2','FC4',...
+    'C3','C1','Cz','C2','C4','CP3','CP1','CPz','CP2','CP4',...
+    'P3','P1','Pz','P2','P4'};
+save('25_centered_electrodes.mat','label');
