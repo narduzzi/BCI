2160e59a68e4acbfc597652d79dd53439df81e38
maro74 <omar.benjellountouimi@epfl.ch>
2017-05-11 08:29:05 +0200

    taking into accound the 25 electrodes

--- Scripts/freqanalysis.m
+++ Scripts/freqanalysis.m
@@ -8,12 +8,15 @@ function freqanalysis(signal_down,condition_text)
 close all
 load(signal_down);
 signal_filtered =  band_filter(1,40,5,256,signal_down);
-electrode = 47;
-[easy hard_assist hard_noassist] = partitioning2(header_down,signal_filtered,condition_text);
+centered_electrodes = load('25_centered_electrodes.mat');
+[indices] = index_of_electrodes(centered_electrodes.label,header_down.Label);
+signal25  = signal_filtered(indices,:);
+signal_avg = mean(signal25,1);
+[easy hard_assist hard_noassist] = partitioning2(header_down,signal_avg,condition_text);
 idx_easy = 0;
-idx_easy = [idx_easy find(easy(electrode,:) == 1e4)];
+idx_easy = [idx_easy find(easy == 1e4)];
 idx_hard_noassist = 0;
-idx_hard_noassist = [idx_hard_noassist find(hard_noassist(electrode,:) == 1e4)];
+idx_hard_noassist = [idx_hard_noassist find(hard_noassist == 1e4)];
 Fs = 256;
 vars = [];
 moy = [];
@@ -21,43 +24,43 @@ moy = [];
 %Frequency representation
 figure(1)
 for i = 1:length(idx_easy)-1
-    condition = easy(electrode,idx_easy(i)+1:idx_easy(i+1)-1);
-    
+    condition = easy(idx_easy(i)+1:idx_easy(i+1)-1);
     L = length(condition);
-    nfft = 2^(nextpow2(L));
-    df = Fs/nfft;
-    f = 0:df:Fs/2;
-    Y = fft(condition,nfft);
-    Y = Y(1:nfft/2 + 1);
-    axis([0 50 0 4000]);
+    Y = fft(condition);
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
     subplot(3,2,i);
     window_size = 50;
-    s_mavg = filter(ones(1,window_size)/window_size, 1, abs(Y));
+    s_mavg = filter(ones(1,window_size)/window_size, 1, P1);
     plot(f,s_mavg);
-    axis([0 50 0 4000]);
+    xlabel('f (Hz)')
+    ylabel('|P1(f)|')
+    axis([0 50 0 0.4]);
     vars(1,i) = var(condition);
     moy(1,i) = mean(condition);
     hold on
     %hard
-    condition = hard_noassist(electrode,idx_hard_noassist(i)+1:idx_hard_noassist(i+1)-1);
+    condition = hard_noassist(idx_hard_noassist(i)+1:idx_hard_noassist(i+1)-1);
     L = length(condition);
-    nfft = 2^(nextpow2(L));
-    df = Fs/nfft;
-    f = 0:df:Fs/2;
-    Y = fft(condition,nfft);
-    Y = Y(1:nfft/2 + 1);
-    axis([0 50 0 4000]);
+    Y = fft(condition);
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
     subplot(3,2,i);
     window_size = 50;
-    s_mavg = filter(ones(1, window_size)/window_size, 1, abs(Y));
+    s_mavg = filter(ones(1, window_size)/window_size, 1, P1);
     plot(f,s_mavg);
-    axis([0 50 0 4000]);
+    xlabel('f (Hz)')
+    ylabel('|P1(f)|')
     vars(2,i) = var(condition);
     moy(2,i) = mean(condition);
-    legend('easy','hard')
-    title(['frequency representation trajectory' int2str(i)])
+    axis([0 50 0 0.4]);
+    legend('Easy','Hard')
+    title(['Frequency representation of trajectory' int2str(i)])
 end
-
 
 figure(2)
 Y = fft(condition);
@@ -77,17 +80,19 @@ trials = [ 1 2 3 4 5];
 plot(trials,vars(1,:));
 hold on;
 plot(trials,vars(2,:));
-legend('easy','hard');
-% hold on;
-% plot(trials,vars(3,:));
-% legend('easy','hard assist','hard');
-
+legend('Easy','Hard');
+title('Variance of the trajectories signal')
+xlabel('Trajectories');
+ylabel('Variance')
 figure(4)
 trials = [ 1 2 3 4 5];
 plot(trials,moy(1,:));
 hold on;
 plot(trials,moy(2,:));
-legend('easy','hard');
+legend('Easy','Hard');
+title('Average of the trajectories signal')
+xlabel('Trajectories');
+ylabel('Average')
 % hold on;
 % plot(trials,moy(3,:));
 % legend('easy','hard assist','hard');
@@ -100,42 +105,41 @@ high = 12.5;
 Fs = 256;
 order = 5;
 window_size = 50;
-axis_size_alpha = [5 15 0 3200];
+axis_size_alpha = [5 15 0 0.4];
 %easy
 figure(5)
 for i = 1:length(idx_easy)-1
-    condition = easy(electrode,idx_easy(i)+1:idx_easy(i+1)-1);
+    condition = easy(idx_easy(i)+1:idx_easy(i+1)-1);
     condition_filtered = band_filter(low,high,order,Fs,condition);
-    avg = 0.5;
     L = length(condition_filtered);
-    nfft = 2^(nextpow2(L));
-    df = Fs/nfft;
-    f = 0:df:Fs/2;
-    Y = fft(condition_filtered,nfft);
-    Y = Y(1:nfft/2 + 1);
+    Y = fft(condition_filtered);
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
     axis(axis_size_alpha);
     subplot(3,2,i);
-    s_mavg = filter(ones(1, window_size)/window_size, 1, abs(Y));
+    s_mavg = filter(ones(1, window_size)/window_size, 1, P1);
     plot(f,s_mavg);
     axis(axis_size_alpha);
     hold on
     
     %hard
-    condition = hard_noassist(electrode,idx_hard_noassist(i)+1:idx_hard_noassist(i+1)-1);
+    condition = hard_noassist(idx_hard_noassist(i)+1:idx_hard_noassist(i+1)-1);
     condition_filtered = band_filter(low,high,order,Fs,condition);
     L = length(condition_filtered);
-    nfft = 2^(nextpow2(L));
-    df = Fs/nfft;
-    f = 0:df:Fs/2;
-    Y = fft(condition_filtered,nfft);
-    Y = Y(1:nfft/2 + 1);
+    Y = fft(condition_filtered);
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
     axis(axis_size_alpha);
     subplot(3,2,i);
-    s_mavg = filter(ones(1, window_size)/window_size, 1, abs(Y));
+    s_mavg = filter(ones(1, window_size)/window_size, 1, P1);
     plot(f,s_mavg);
     axis(axis_size_alpha);
-    legend('easy','hard');
-    title(['alpha band trajectory' int2str(i)]);
+    legend('Easy','Hard');
+    title(['Alpha band of trajectory' int2str(i)]);
 end
 figure(6)
 window_size = 256;
@@ -147,40 +151,40 @@ low = 12.5;
 high = 30;
 Fs = 256;
 order = 5;
-axis_size_beta=[10 35 0 2000];
+axis_size_beta=[10 35 0 0.1];
 
 figure(7)
 for i = 1:length(idx_easy)-1
     %easy
-    condition = easy(electrode,idx_easy(i)+1:idx_easy(i+1)-1);
+    condition = easy(idx_easy(i)+1:idx_easy(i+1)-1);
     condition_filtered = band_filter(low,high,order,Fs,condition);
     L = length(condition_filtered);
-    nfft = 2^(nextpow2(L));
-    df = Fs/nfft;
-    f = 0:df:Fs/2;
-    Y = fft(condition_filtered,nfft);
-    Y = Y(1:nfft/2 + 1);
+    Y = fft(condition_filtered);
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
     axis(axis_size_beta);
     subplot(3,2,i);
-    s_mavg = filter(ones(1, window_size)/window_size, 1, abs(Y));
+    s_mavg = filter(ones(1, window_size)/window_size, 1, P1);
     plot(f,s_mavg);
     axis(axis_size_beta);
     hold on
     
     %hard
-    condition = hard_noassist(electrode,idx_hard_noassist(i)+1:idx_hard_noassist(i+1)-1);
+    condition = hard_noassist(idx_hard_noassist(i)+1:idx_hard_noassist(i+1)-1);
     condition_filtered = band_filter(low,high,order,Fs,condition);
     L = length(condition_filtered);
-    nfft = 2^(nextpow2(L));
-    df = Fs/nfft;
-    f = 0:df:Fs/2;
-    Y = fft(condition_filtered,nfft);
-    Y = Y(1:nfft/2 + 1);
+    Y = fft(condition_filtered);
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
     axis(axis_size_beta);
     subplot(3,2,i);
-    s_mavg = filter(ones(1, window_size)/window_size, 1, abs(Y));
+    s_mavg = filter(ones(1, window_size)/window_size, 1, P1);
     plot(f,s_mavg);
     axis(axis_size_beta);
-    legend('easy','hard');
-    title(['beta band trajectory' int2str(i)])
+    legend('Easy','Hard');
+    title(['Beta band trajectory' int2str(i)])
 end
