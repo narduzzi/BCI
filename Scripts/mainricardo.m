1263bfe0ddd1118fc307837192ade8c7445e3997
RickCM <rick@citycable.ch>
2017-06-01 19:47:26 +0200

    started extraction of features online

--- Scripts/mainricardo.m
+++ Scripts/mainricardo.m
@@ -5,7 +5,7 @@ clc
 
 %%
 
-load('data_ricardo1.mat')
+load('data_loic1.mat')
 downfactor = 8;
 low=1;
 high=40;
@@ -14,15 +14,17 @@ Fs = header_down.SampleRate/downfactor;
 signal_filtered = band_filter(low,high,order,Fs,signal_down);
 
 %%
-text = 'data_ricardo1_ses_1_condition.txt';
+text = 'data_loic1_ses_1_condition.txt';
 [easy,hard_assist,hard_noassist] = partitioning2(header_down,signal_filtered,text);
 
 %%
 centered_electrodes = load('25_centered_electrodes.mat');
-%[indices] = index_of_electrodes(centered_electrodes.label,header_down);
-indices = 1:64;
-%indices = [28, 29, 35, 42, 43, 44, 52, 53, 60, 61, 62]; %loic easy>hard
-%indices = [18, 31, 32, 39, 40, 47, 48, 50, 55, 56, 63, 64];
+[indices] = index_of_electrodes(centered_electrodes.label,header_down);
+%indices = 1:64;
+%indices = [1, 19, 21, 23, 24, 27, 28, 29, 33, 35, 36, 49, 60, 63, 64, 18, 31, 32, 39, 40, 42, 43, 44, 47, 48, 50, 52, 55, 56, 57, 61 ];
+%indices = [18, 27, 28, 29, 31, 32, 48, 49, 50, 56, 60, 61];
+indices = sort(indices);
+
 %%
 window_size = 200;
 step_size = window_size/2;
@@ -34,7 +36,7 @@ train_errors = [];
 
 test_errors = [];
 for traj = 0:4
-    [train_labels,train_features,test_labels,test_features] = create_folds(features_selected,traj);
+    [train_labels,train_features,test_labels,test_features] = create_folds(features_extracted,traj);
     classifier_lda = fitcdiscr(train_features, train_labels, 'DiscrimTyp', 'Linear', 'Prior', 'uniform');
     
     yhat_lda = predict(classifier_lda, train_features); 
@@ -57,7 +59,7 @@ fprintf('Mean Test Error : %0.3f\n',mean(test_errors));
 features_selected = [features_extracted(:,1:2) features_pca(:,1:100)];
 %features_selected = features_pca(:,1:876);
 
-% 
+ 
 % %% Testing with 2nd session
 % %%
 % clc
@@ -65,7 +67,7 @@ features_selected = [features_extracted(:,1:2) features_pca(:,1:100)];
 % close all
 % %%
 % 
-% load('data_ricardo2.mat')
+% load('data_loic2.mat')
 % downfactor = 8;
 % low=1;
 % high=40;
@@ -75,9 +77,11 @@ features_selected = [features_extracted(:,1:2) features_pca(:,1:100)];
 % 
 % %%
 % [easy_test, medium_test, hard_test] = partitioning_testsession(header_down, signal_filtered);
-% %centered_electrodes = load('25_centered_electrodes.mat');
-% %[indices] = index_of_electrodes(centered_electrodes.label,header_down);
-% indices = 1:64;
+% centered_electrodes = load('25_centered_electrodes.mat');
+% [indices] = index_of_electrodes(centered_electrodes.label,header_down);
+% %indices = [18, 27, 28, 29, 31, 32, 48, 49, 50, 56, 60, 61];
+% indices = sort(indices);
+% %indices = 1:64;
 % %%
 % window_size = 200;
 % step_size = window_size/2;
@@ -94,16 +98,16 @@ features_selected = [features_extracted(:,1:2) features_pca(:,1:100)];
 % fprintf('Test Error : %0.3f\n ',test_err);
 % C = confusionmat(test_labels, yhat_lda)
 % 
-% %% Test with PCA
-% load('classifier4.mat');
-% 
-% test_features = features_extracted(:,3:end);
-% test_labels = features_extracted(:,1);
-% [coeff, features_pca, variance] = pca(test_features);
-% test_features = features_pca(:,1:100);
-% yhat_lda = predict(classifier_lda, test_features);
-% [test_err] = classerror(test_labels, yhat_lda); 
-% 
-% fprintf('Test Error : %0.3f\n ',test_err);
-% C = confusionmat(test_labels, yhat_lda)
+% % %% Test with PCA
+% % load('classifier_test.mat');
+% % 
+% % test_features = features_extracted(:,3:end);
+% % test_labels = features_extracted(:,1);
+% % [coeff, features_pca, variance] = pca(test_features);
+% % test_features = features_pca(:,1:100);
+% % yhat_lda = predict(classifier_lda, test_features);
+% % [test_err] = classerror(test_labels, yhat_lda); 
+% % 
+% % fprintf('Test Error : %0.3f\n ',test_err);
+% % C = confusionmat(test_labels, yhat_lda)
 
