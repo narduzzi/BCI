80b246e8f91108666d6f2b3a2f641ea1ddfcdb81
RickCM <rick@citycable.ch>
2017-06-12 16:10:14 +0200

    modifi

--- Scripts/discriminant_analysis.m
+++ Scripts/discriminant_analysis.m
@@ -4,7 +4,7 @@ function discriminant_analysis(features, nb_features)
 
 
 
-for traj=1:4
+for traj=0:4
     disp(['Cross validation fold number ' int2str(traj)])
     %Determine the test and train tests
     [train_labels,train_features,test_labels,test_features] = create_folds(features,traj);
@@ -15,15 +15,15 @@ for traj=1:4
         disp(['Feature: ' int2str(i)])
         classifier_lda = fitcdiscr(train_features(:,orderedInd(1:i)), train_labels, 'DiscrimTyp', 'Linear', 'Prior', 'uniform');
         yhat_lda = predict(classifier_lda, train_features(:,orderedInd(1:i))); 
-        training_error_lda(traj,j) = classerror(train_labels, yhat_lda);
+        training_error_lda(traj+1,j) = classerror(train_labels, yhat_lda);
         yhat_lda = predict(classifier_lda, test_features(:,orderedInd(1:i)));
-        testing_error_lda(traj,j) = classerror(test_labels, yhat_lda); 
+        testing_error_lda(traj+1,j) = classerror(test_labels, yhat_lda); 
 
         classifier_dlda = fitcdiscr(train_features(:,orderedInd(1:i)), train_labels, 'DiscrimTyp', 'DiagLinear', 'Prior', 'uniform');
         yhat_dlda = predict(classifier_dlda, train_features(:,orderedInd(1:i))); 
-        training_error_dlda(traj,j) = classerror(train_labels, yhat_dlda);
+        training_error_dlda(traj+1,j) = classerror(train_labels, yhat_dlda);
         yhat_dlda = predict(classifier_dlda, test_features(:,orderedInd(1:i)));
-        testing_error_dlda(traj,j) = classerror(test_labels, yhat_dlda); 
+        testing_error_dlda(traj+1,j) = classerror(test_labels, yhat_dlda); 
 
         %     classifier_qda = fitcdiscr(train_features(:,orderedInd(1:i)), train_labels, 'DiscrimTyp', 'Quadratic', 'Prior', 'uniform');
         %     yhat_qda = predict(classifier_qda, train_features(:,orderedInd(1:i))); 
@@ -33,27 +33,27 @@ for traj=1:4
 
         classifier_dqda = fitcdiscr(train_features(:,orderedInd(1:i)), train_labels, 'DiscrimTyp', 'DiagQuadratic', 'Prior', 'uniform');
         yhat_dqda = predict(classifier_dqda, train_features(:,orderedInd(1:i))); 
-        training_error_dqda(traj,j) = classerror(train_labels, yhat_dqda);
+        training_error_dqda(traj+1,j) = classerror(train_labels, yhat_dqda);
         yhat_dqda = predict(classifier_dqda, test_features(:,orderedInd(1:i)));
-        testing_error_dqda(traj,j) = classerror(test_labels, yhat_dqda);
+        testing_error_dqda(traj+1,j) = classerror(test_labels, yhat_dqda);
         
         SVMModel_linear = fitcsvm(train_features(:,orderedInd(1:i)),train_labels,'KernelFunction','linear');
         [predicted_train,score] = predict(SVMModel_linear,train_features(:,orderedInd(1:i)));
-        training_error_SVM_linear(traj,j) = classerror(train_labels,predicted_train);
+        training_error_SVM_linear(traj+1,j) = classerror(train_labels,predicted_train);
         [predicted_labels,score] = predict(SVMModel_linear,test_features(:,orderedInd(1:i)));
-        test_error_SVM_linear(traj,j) = classerror(test_labels,predicted_labels);
+        test_error_SVM_linear(traj+1,j) = classerror(test_labels,predicted_labels);
         
         SVMModel_quadratic = fitcsvm(train_features(:,orderedInd(1:i)),train_labels,'KernelFunction','polynomial');
         [predicted_train,score] = predict(SVMModel_quadratic,train_features(:,orderedInd(1:i)));
-        training_error_SVM_quadratic(traj,j) = classerror(train_labels,predicted_train);
+        training_error_SVM_quadratic(traj+1,j) = classerror(train_labels,predicted_train);
         [predicted_labels,score] = predict(SVMModel_quadratic,test_features(:,orderedInd(1:i)));
-        test_error_SVM_quadratic(traj,j) = classerror(test_labels,predicted_labels);
+        test_error_SVM_quadratic(traj+1,j) = classerror(test_labels,predicted_labels);
         
         SVMModel_rbf = fitcsvm(train_features(:,orderedInd(1:i)),train_labels,'KernelFunction','rbf');
         [predicted_train,score] = predict(SVMModel_rbf,train_features(:,orderedInd(1:i)));
-        training_error_SVM_rbf(traj,j) = classerror(train_labels,predicted_train);
+        training_error_SVM_rbf(traj+1,j) = classerror(train_labels,predicted_train);
         [predicted_labels,score] = predict(SVMModel_rbf,test_features(:,orderedInd(1:i)));
-        test_error_SVM_rbf(traj,j) = classerror(test_labels,predicted_labels);
+        test_error_SVM_rbf(traj+1,j) = classerror(test_labels,predicted_labels);
     end
 end
 
