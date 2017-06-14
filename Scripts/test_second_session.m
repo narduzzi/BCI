3647d4a478977bf23d4489876463efdddc5acae6
RickCM <rick@citycable.ch>
2017-06-14 11:02:12 +0200

    minor changes

---
+++ Scripts/test_second_session.m
@@ -0,0 +1,38 @@
+clc;
+clear all; 
+close all;
+
+load('data_ricardo2.mat')
+features_extracted = process_session2(signal_down, header_down);
+
+test_features = features_extracted(:,3:end);
+test_labels = features_extracted(:,1);
+
+load('classifier_ricardo_relieff.mat');
+nb_features = model_ricardo.nb_features;
+indices = model_ricardo.indices;
+classifier = model_ricardo.classifier;
+[yhat, score, autre] = predict(classifier, test_features(:,indices)); 
+
+[test_err] = classerror(test_labels, yhat); 
+fprintf('Test Error : %0.3f\n ',test_err);
+C = confusionmat(test_labels, yhat)
+[X,Y,T,AUC] = perfcurve(test_labels, score(:,2), 1);
+figure(1)
+plot(X,Y)
+
+% %% Test with PCA
+% load('classifier_test.mat');
+% 
+% test_features = features_extracted(:,3:end);
+% test_labels = features_extracted(:,1);
+% [coeff, features_pca, variance] = pca(test_features);
+% test_features = features_pca(:,1:100);
+% yhat_lda = predict(classifier_lda, test_features);
+% [test_err] = classerror(test_labels, yhat_lda); 
+% 
+% fprintf('Test Error : %0.3f\n ',test_err);
+% C = confusionmat(test_labels, yhat_lda)
+
+
+
