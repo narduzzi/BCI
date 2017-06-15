178e3369209075cb0fbf89031ed3df07feed932f
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-06-15 17:11:56 +0200

    Medium testing

--- Scripts/basic_pipeline.m
+++ Scripts/basic_pipeline.m
@@ -16,11 +16,79 @@ features_session2(find(features_session2(:,1)==2),:) = [];
 trajectory_out_train = -1;
 [train_label,train_features,val_label,val_features] = create_folds(features_session1,trajectory_out_train);
 [test_label,test_features,n1,n2] = create_folds(features_session2,-1);
-[medium_label,medium_features,n1,n2] = create_folds(features_mediums_session2,-1);
+[test_labels_medium,test_features_medium,n1,n2] = create_folds(features_mediums_session2,-1);
 
-%%Train models here
+%%Models here
 
-%Test models here
+%Fisher
+%Training DLDA
+nb_features = 460;
+disp('Computing Fisher..');
+[orderedInd, orderedPower] = rankfeat(train_features, train_labels, 'fisher');
+disp('Training session 1');
+classifier_dlda = fitcdiscr(train_features(:,orderedInd(1:nb_features)), train_labels, 'DiscrimTyp', 'DiagLinear', 'Prior', 'uniform');
+%Testing
+[predicted_train,score] = predict(classifier_dlda, train_features(:,orderedInd(1:nb_features))); 
+training_error_dlda = classerror(train_labels, predicted_train);
+disp('Testing on session 2');
+[predicted_labels,score]= predict(classifier_dlda, test_features(:,orderedInd(1:nb_features)));
+testing_error_dlda = classerror(test_labels, predicted_labels); 
+
+[X,Y,T,AUC_dlda] = perfcurve(test_labels, score(:,2), 1);
+
+disp(fprintf('Train error on session 1 : %0.4f \n',training_error_dlda));
+disp(fprintf('Test error on session 2 : %0.4f \n',testing_error_dlda));
+disp(fprintf('AUC : %0.4f \n',AUC_dlda));
+
+%%
+%ReliefF
+%Training SVM linear
+disp('Computing ReliefF');
+nb_features= 801;
+K=400;
+[orderedInd, orderedPower] = relieff(train_features, train_labels, K);
+%Testing
+classifier_svml = fitcsvm(train_features(:,orderedInd(1:nb_features)),train_labels,'KernelFunction','linear');
+[predicted_train,score] = predict(classifier_svml, train_features(:,orderedInd(1:nb_features))); 
+training_error_svml = classerror(train_labels, predicted_train);
+[predicted_test,score] = predict(classifier_svml, test_features(:,orderedInd(1:nb_features)));
+testing_error_svml = classerror(test_labels, predicted_test); 
+
+[X,Y,T,AUC_SVM] = perfcurve(test_labels, score(:,2), 1);
+
+disp(fprintf('Train error on session 1 : %0.4f \n',training_error_svml));
+disp(fprintf('Test error on session 2 : %0.4f \n',testing_error_svml));
+disp(fprintf('AUC : %0.4f \n',AUC_SVM));
+
+
+%PCA
+disp('PCA:')
+opt_PCs = 330;
+[coeff, train_PCA, variance] = pca(train_features);
+% Attention: We have to center the testing data also
+mean_t = mean(train_features,1);
+for i=1:size(test_features,1)
+    mean_test(i,:) = mean_t;
+end
+test_features_centered = test_features - mean_test;
+test_PCA = test_features_centered * coeff;
+
+classifier = fitcdiscr(train_PCA(:,1:opt_PCs), train_labels, 'DiscrimTyp', 'DiagQuadratic', 'Prior', 'uniform');
+yhat = predict(classifier, train_PCA(:,1:opt_PCs)); 
+training_error_PCA = classerror(train_labels, yhat);
+
+%Test
+[yhat, score_PCA] = predict(classifier, test_PCA(:,1:opt_PCs));
+testing_error_PCA = classerror(test_labels, yhat);
+[X,Y,T,AUC_PCA] = perfcurve(test_labels, score_PCA(:,2), 1);
+
+disp(fprintf('Train error on session 1 : %0.4f \n',training_error_PCA));
+disp(fprintf('Test error on session 2 : %0.4f \n',testing_error_PCA));
+disp(fprintf('AUC : %0.4f \n',AUC_PCA));
 
 %Test medium classification here
+%Fisher
 
+%Relief
+
+%PCA
