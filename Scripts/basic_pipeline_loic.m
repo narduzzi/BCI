9e42c357db962c22188963836d793ad6447c6260
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-06-15 17:52:29 +0200

    bug fix

--- Scripts/basic_pipeline_loic.m
+++ Scripts/basic_pipeline_loic.m
@@ -2,9 +2,9 @@
 
 %Pipeline : Processing (CAR,Downsampling) and feature extraction
 features_session1 = process_recording('Recordings/af6_15032017_loic1/biosemi/data_loic1.bdf',...
-    'Recordings/af6_15032017_loic1/unity/af6_15032017_ses_1_condition.txt',1)
+    'Recordings/af6_15032017_loic1/unity/af6_15032017_ses_1_condition.txt',1);
 features_session2 = process_recording('Recordings/af6_12042017_loic2/biosemi/data_loic2.bdf',...
-    'Recordings/af6_12042017_loic2/unity/af6_12042017_ses_2_condition.txt',2)
+    'Recordings/af6_12042017_loic2/unity/af6_12042017_ses_2_condition.txt',2);
 
 %%
 %Separation
@@ -14,11 +14,12 @@ features_session2(find(features_session2(:,1)==2),:) = [];
 
 %Create folds
 trajectory_out_train = -1;
-[train_label,train_features,val_label,val_features] = create_folds(features_session1,trajectory_out_train);
-[test_label,test_features,n1,n2] = create_folds(features_session2,-1);
+[train_labels,train_features,val_label,val_features] = create_folds(features_session1,trajectory_out_train);
+[test_labels,test_features,n1,n2] = create_folds(features_session2,-1);
 [test_labels_medium,test_features_medium,n1,n2] = create_folds(features_mediums_session2,-1);
 
 %%Models here
+
 %%
 %Fisher
 %Training DLDA
