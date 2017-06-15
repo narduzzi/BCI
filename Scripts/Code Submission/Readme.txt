5078e874bb3d7891ecfb60b7a6b7a08dc5c9851c
maro74 <omar.benjellountouimi@epfl.ch>
2017-06-15 20:25:23 +0200

    voilàààààààà

--- Scripts/Code Submission/Readme.txt
+++ Scripts/Code Submission/Readme.txt
@@ -7,9 +7,20 @@ Two types of codes can be found:
 Small description of the codes:
 
 1) Codes for processing the signal, epoching, epoching, feature extraction, building, testing and evaluation of the models
-For each subject, a matlab script containing the whole pipeline from the signal processing to the ?? was established.
 
-2) Codes on the raw signal or with the ???
+For each subject, a matlab script containing the whole pipeline from the signal processing to the test of the models on the second
+sesssion. This can be done by the scripts "basi_pipeline_'subject'", each subject having his own models. The processing is basically the
+same only the models differ. This script takes the data from the two sessions process them and train on the first session
+and test on the second. ROC curves, class errors and AUC are provided. The test with the medium condition is also performed
+and the results are given as persentage classified in easy or hard. To load the data ensure that the files are located at
+the right place.
+The functions model_PCA, model_fisher and model_reliefF can be used to perform cross validation on different feature selection
+methods with 3 discriminant classifiers and 3 SVM classifiers, they also plot the results of the mean errors as a function of 
+the features.
+scatte_plot_features can be used to have a feature anlysis of the train set, namely the first session.
+
+
+2) Codes on the raw signal or for the behavioral analysis
 For generating plots showing the difficulty perceived by the subjects, run the code "self_assess". 
 For generating plots showing statistics on temporal aspects of the experiment, run the code "trajectory_time". 
 For plotting the success rate of a subject, run the code "plotSucessRate". 
---
+++ Scripts/Code Submission/basic_pipeline_loic.m
@@ -0,0 +1,115 @@
+%Basic model
+
+%Pipeline : Processing (CAR,Downsampling) and feature extraction
+features_session1 = process_recording('Recordings/af6_15032017_loic1/biosemi/data_loic1.bdf',...
+    'Recordings/af6_15032017_loic1/unity/af6_15032017_ses_1_condition.txt',1)
+features_session2 = process_recording('Recordings/af6_12042017_loic2/biosemi/data_loic2.bdf',...
+    'Recordings/af6_12042017_loic2/unity/af6_12042017_ses_2_condition.txt',2)
+
+
+%%
+%Evaluating models
+train_features = features_session1(:,3:end);
+train_labels = features_session1(:,1);
+%collecting test data with medium condition
+test_features_medium = features_session2(:,3:end);
+
+%remove medium condition
+test_features= test_features_medium;
+test_features(find(features_session2(:,1)==2),:) = [];
+
+%defining test labels
+test_labels_medium = features_session2(:,1);
+test_labels = test_labels_medium;
+%removing the medium labels
+test_labels(find(test_labels==2)) = [];
+%%
+%Fisher model: 
+disp('Evaluating models on second session...')
+disp('Fisher method:')
+opt_features = 460;
+%Train
+[orderedInd, orderedPower] = rankfeat(train_features,train_labels, 'fisher');
+classifier = fitcdiscr(train_features(:,orderedInd(1:opt_features)), train_labels, 'DiscrimTyp', 'DiagLinear', 'Prior', 'uniform');
+yhat = predict(classifier, train_features(:,orderedInd(1:opt_features))); 
+training_error_fisher = classerror(train_labels, yhat);
+
+%Test
+[yhat, score_fisher] = predict(classifier, test_features(:,orderedInd(1:opt_features)));
+testing_error_fisher = classerror(test_labels, yhat);
+[X,Y,T,AUC_fisher] = perfcurve(test_labels, score_fisher(:,2), 1);
+disp(['AUC of ' num2str(AUC_fisher) ' for fisher.']);
+disp(['test error of ' num2str(testing_error_fisher) ' for fisher.']);
+
+[yhat, score_fisher] = predict(classifier, test_features_medium(:,orderedInd(1:opt_features)));
+[ percentage_medium_in_easy_fisher, percentage_medium_in_hard_fisher ] = medium_testing( yhat, test_labels_medium);
+%Plot ROC curve
+figure(1)
+plot(X,Y)
+xlabel('False positive rate')
+ylabel('True positive rate')
+title('ROC for classification using Fisher')
+%%
+%ReliefF : 
+disp('Relieff method:')
+%Train
+opt_features = 801;
+[RANKED,WEIGHT] = relieff(train_features,train_labels,400);
+classifier = fitcsvm(train_features(:, RANKED(1:opt_features)), train_labels, 'KernelFunction','linear');
+yhat = predict(classifier, train_features(:,RANKED(1:opt_features))); 
+training_error_relief = classerror(train_labels, yhat);
+
+%Test
+[yhat, score_relief] = predict(classifier, test_features(:,RANKED(1:opt_features)));
+testing_error_relief = classerror(test_labels, yhat);
+[X,Y,T,AUC_relieff] = perfcurve(test_labels, score_relief(:,2), 1);
+disp(['AUC of ' num2str(AUC_relieff) ' for relieff.']);
+disp(['test error of ' num2str(testing_error_relief) ' for reliefF.']);
+
+%Evaluate medium
+[yhat, score_relief] = predict(classifier, test_features_medium(:,RANKED(1:opt_features)));
+[ percentage_medium_in_easy_relief, percentage_medium_in_hard_relief ] = medium_testing( yhat, test_labels_medium);
+%Plot ROC curve
+figure(2)
+plot(X,Y)
+xlabel('False positive rate')
+ylabel('True positive rate')
+title('ROC for classification using ReliefF')
+%%
+%PCA : 
+disp('PCA:')
+opt_PCs = 330;
+[coeff, train_PCA, variance] = pca(train_features);
+% Attention: We have to center the testing data also
+mean_t = mean(train_features,1);
+for i=1:size(test_features,1)
+    mean_test(i,:) = mean_t;
+end
+test_features_centered = test_features - mean_test;
+test_PCA = test_features_centered * coeff;
+
+classifier = fitcdiscr(train_PCA(:,1:opt_PCs), train_labels, 'DiscrimTyp', 'DiagQuadratic', 'Prior', 'uniform');
+yhat = predict(classifier, train_PCA(:,1:opt_PCs)); 
+training_error_PCA = classerror(train_labels, yhat);
+
+%Test
+[yhat, score_PCA] = predict(classifier, test_PCA(:,1:opt_PCs));
+testing_error_PCA = classerror(test_labels, yhat);
+[X,Y,T,AUC_PCA] = perfcurve(test_labels, score_PCA(:,2), 1);
+disp(['AUC of ' num2str(AUC_PCA) ' for PCA.']);
+disp(['test error of ' num2str(testing_error_PCA) ' for PCA.']);
+figure(3)
+plot(X,Y)
+xlabel('False positive rate')
+ylabel('True positive rate')
+title('ROC for classification using PCA')
+
+
+%Evaluate medium
+for i=1:size(test_features_medium,1)
+    mean_test(i,:) = mean_t;
+end
+test_features_centered = test_features_medium - mean_test;
+test_PCA_medium = test_features_centered * coeff;
+[yhat, score_PCA] = predict(classifier, test_PCA_medium(:,1:opt_PCs));
+[percentage_medium_in_easy_PCA, percentage_medium_in_hard_PCA ] = medium_testing( yhat, test_labels_medium);
