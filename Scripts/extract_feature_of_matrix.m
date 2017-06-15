7a86b86f74acab2a8bf05903a4c7cf6a25b69d93
maro74 <omar.benjellountouimi@epfl.ch>
2017-06-15 16:22:31 +0200

    removing stats

--- Scripts/extract_feature_of_matrix.m
+++ Scripts/extract_feature_of_matrix.m
@@ -42,51 +42,10 @@ function extracted = extract_feature_of_matrix(matrix,windows_size,difficulty)
         
         s = size(frequency_coeff);
         new_size = [1,s(1)*s(2)];
-%         freq_coeff = [];
-%         for i=1:last_electrode_indice
-%             freq_coeff = [freq_coeff frequency_coeff(i,:)];
-%         end
-        frequency_coeff = reshape(frequency_coeff,new_size);
 
-        %{
-        %extract energy
-        energy = periodogram(signal);
-        s = size(energy);
-        new_size = [1, s(1)*s(2)];
-        energy = reshape(energy,new_size);
-        %}
-        
-        %extract statistics
-        statistics = [];
-        smin = min(signal,[],2);
-        smean = mean(signal,2);
-        smedian = median(signal,2);
-        smax = max(signal,[],2);
-        svar = var(signal,[],2);
-        smost_frequent = mode(signal,2);
-        sdev = std(signal,[],2);
-        mav = MAV(signal);
-        wl = wavelength(signal);
-        zc = zerocross(signal);
-        statistics(1,:) = smedian';
-        statistics(2,:) = svar';
-        statistics(3,:) = sdev';
-        statistics(4,:) = mav';
-        statistics(5,:) = wl';        
-        statistics(6,:) = zc';
-        % Stats not appropriate
-%         statistics(7,:) = smax';
-%         statistics(8,:) = smin';
-%         statistics(9,:) = smost_frequent';
-%         statistics(10,:) = smean';
-        
-        labels_stat = {'min','mean','median','max','var','most frequent value','dev'};
-        
-        s = size(statistics);
-        new_size = [1, s(1)*s(2)];
-        statistics = reshape(statistics,new_size);
+        frequency_coeff = reshape(frequency_coeff,new_size);          
+    
 
-%         features = [difficulty trajectory_num statistics];
         features = [difficulty trajectory_num frequency_coeff];
 
         features_matrix = vertcat(features_matrix,features);
