4d4de8e2ce863f2a2c8e2681a995c6ecbbe5a862
maro74 <omar.benjellountouimi@epfl.ch>
2017-06-15 21:01:05 +0200

    j'en ai maaaaaarrrre

---
+++ Scripts/Code Submission/process_recording.m
@@ -0,0 +1,33 @@
+function features_extracted = process_recording(bdf_path,condition_path,type)
+
+    path=bdf_path;
+    text=condition_path
+    %% SESSION 1
+    rawsignal = path;
+    downfactor = 8;
+    sampling_freq = 2048;
+    low=1;
+    high=40;
+    order=5;
+
+    %Main part
+    disp('Loading data...')
+    [signal,header] = sload(rawsignal);
+    signal = signal';
+    %channel selection
+    signal = signal(1:64,:);
+
+    disp(fprintf('Downsampling : Factor %0.0f \n',downfactor))
+    [header_down,signal_down] = downsampling(header, signal,downfactor);
+
+    disp('Applying car...');
+    %signal_filtered = car(signal_filtered);
+    signal_down = car(signal_down);
+    disp('CAR done.');
+    %%
+    if(type == 1)
+        features_extracted = process_session1(signal_down,header_down,text);
+    else
+         features_extracted = process_session2(signal_down,header_down);
+    end
+end
