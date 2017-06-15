568d8cb5b24f4615c6964fca5da696e88801f427
maro74 <omar.benjellountouimi@epfl.ch>
2017-06-15 17:32:05 +0200

    adding codes for submission

---
+++ Scripts/Code Submission/behavioral_analysis.m
@@ -0,0 +1,81 @@
+function behavioral_analysis()
+% Function that performs all the behavioral analysis without distinction of
+% the subjects in order to achieve statistical signifiance. 
+
+%% 1: Determine the self-assessement for all subjects mixed
+easy = [];
+hard = [];
+hard_ass = [];
+subjectNames = {'omar1','loic1','ricardo1','simon1'};
+for i=1:4
+    [easy_sub, hard_sub, hard_ass_sub] = self_assess(subjectNames{i});
+    easy = vertcat(easy, easy_sub);
+    hard = vertcat(hard, hard_sub);
+    hard_ass = vertcat(hard_ass, hard_ass_sub);
+end
+close all;
+figure(1)
+boxplot([easy,hard_ass, hard],'Notch', 'on','Labels',{'easy','hard + assist', 'hard'})
+title('Workload perceived by the subjects')
+ylabel('Scale of difficulty [0-100]');
+
+%% 2 Determine the user performance for all subjects
+
+easy = [];
+hard = [];
+hard_ass = [];
+for i=1:4
+    file_data = strcat('data_', subjectNames{i});
+    file_cond = strcat(file_data, '_ses_1_condition.txt');
+    condition = text2matrix(file_cond);
+    load(strcat(file_data, '.mat'));
+    [matrix_success, rate_success] = user_performance(header_down.EVENT.TYP);
+
+    easy = [easy rate_success(condition == 0)];
+    hard_ass = [hard_ass rate_success(condition == 1)];
+    hard = [hard rate_success(condition == 2)];
+end
+
+figure(2)
+boxplot([easy', hard_ass', hard'],'Notch', 'on','Labels',{'easy','hard + assist', 'hard'})
+title('Users performance')
+ylabel('Rate of success');
+
+ 
+
+%% 3 Determine the time spent per trajectory and per waypoint depending on the condition
+
+t_easy = [];
+t_hard_ass = [];
+t_hard = [];
+tw_easy = [];
+tw_hard_ass = [];
+tw_hard = [];
+for i=1:4
+    file_data = strcat('data_', subjectNames{i});
+    file_cond = strcat(file_data, '_ses_1_condition.txt');
+    condition = text2matrix(file_cond);
+    load(strcat(file_data, '.mat'));
+    [time_easy, time_hardassist, time_hardnoassist, time_waypoint_easy, time_waypoint_hardassist, time_waypoint_hardnoassist] = trajectory_time( header_down, subjectNames{i});
+
+    t_easy = [t_easy time_easy];
+    t_hard_ass = [t_hard_ass time_hardassist];
+    t_hard = [t_hard time_hardnoassist];
+    
+    tw_easy = [tw_easy time_waypoint_easy];
+    tw_hard_ass = [tw_hard_ass time_waypoint_hardassist];
+    tw_hard = [tw_hard time_waypoint_hardnoassist];  
+end
+
+close all;
+figure(3)
+boxplot([time_easy', time_hardassist', time_hardnoassist'],'Notch', 'on','Labels',{'easy','hard + assist', 'hard'})
+title('Time spent per trajectory for each condition')
+ylabel('Time [s]');
+
+figure(4)
+boxplot([time_waypoint_easy', time_waypoint_hardassist', time_waypoint_hardnoassist'],'Notch', 'on','Labels',{'easy','hard + assist', 'hard'})
+title('Time spent between two waypoints for each condition')   
+ylabel('Time [s]');
+
+end
---
+++ Scripts/Code Submission/create_folds.m
@@ -0,0 +1,25 @@
+function [train_labels,train_features,test_labels,test_features] = create_folds(features_list,index_test)
+%This function create folds. The trajectory == index_test is retained as
+%test fold.
+    shape = size(features_list);
+    num_samples = shape(1);
+
+    train_labels = [];
+    train_features = [];
+    test_labels = [];
+    test_features = [];
+
+    for i=1:num_samples
+        sample = features_list(i,:);
+        sample_label = sample(1);
+        sample_features = sample(3:shape(2));
+
+        if(sample(2)==index_test)
+            test_labels = vertcat(test_labels,sample_label);
+            test_features = vertcat(test_features,sample_features);
+        else
+            train_labels = vertcat(train_labels,sample_label);
+            train_features = vertcat(train_features,sample_features);
+        end
+    end
+end
